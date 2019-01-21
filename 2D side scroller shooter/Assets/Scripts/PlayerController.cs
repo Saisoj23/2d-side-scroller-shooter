9b69e9b17e57455aa69264ad2945eb0ae83bdf21
Saisoj23 <30736357+Saisoj23@users.noreply.github.com>
2019-01-21 11:41:57 -0300

    Before the groundNormal hell

--- 2D side scroller shooter/Assets/Scripts/PlayerController.cs
+++ 2D side scroller shooter/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     float hMovement;
     LayerMask playerLayer;
     Vector3 velocity;
+    Vector3 groundNormal;
 
     Rigidbody2D rb;
     Transform rayL;
@@ -22,12 +23,14 @@ public class PlayerController : MonoBehaviour
     public float movementAceleration;
     public float movementDesaceleracion;
     public float movementSensivility;
+    public Vector2 maxWalkAngle;
     [Header("Jump and Gravity")]
     public float gravity;
     public float jumpSpeed;
     public float jumpTime;
     public float jumpAceleration;
     public float jumpDesaceleration;
+    public float groundcheckDistance;
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -39,27 +42,54 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         hMovement = Input.GetAxisRaw("Horizontal");
-        hitL = Physics2D.Raycast(rayL.position, new Vector2(0f,-1f), 0.05f, ~playerLayer);
-        hitR = Physics2D.Raycast(rayR.position, new Vector2(0f,-1f), 0.05f, ~playerLayer);
+        if (grounded && Input.GetButtonDown("Jump"))
+        {
+            jumping = true;
+            jumpingTime = 0f;
+        } 
+        else if (!grounded && Input.GetButton("Jump"))
+        {
+            jumpingTime += Time.deltaTime;
+        }
+        if (Input.GetButtonUp("Jump") || jumpingTime > jumpTime)
+        {
+            jumping = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //GrounCheck
+        hitL = Physics2D.Raycast(rayL.position, new Vector2(0f,-1f), groundcheckDistance, ~playerLayer);
+        hitR = Physics2D.Raycast(rayR.position, new Vector2(0f,-1f), groundcheckDistance, ~playerLayer);
         if (hitL.collider != null || hitR.collider != null)
         {
             grounded = true;
+            velocity.y -= hitL.collider != null ? rayL.position.y + hitL.point.y : rayR.position.y + hitR.point.y;
+            if(hitL.normal != new Vector2(0f, 1f))
+            {
+                groundNormal = Quaternion.AngleAxis(90f, hitL.normal).eulerAngles;
+            }
+            else if (hitR.normal != new Vector2(0f, 1f))
+            {
+                groundNormal = Quaternion.AngleAxis(-90f, hitR.normal).eulerAngles;
+            }
+            else
+            {
+                groundNormal = Vector2.right;
+            }
         } 
         else
         {
             grounded = false;
         }
-        Debug.Log(grounded);
-    }
-
-    void FixedUpdate()
-    {
+        Debug.Log("left: " + hitL.normal +"  right: " + hitR.normal);
         //Horizontal Move
         if (Mathf.Abs(hMovement) > movementSensivility)
         {
             if (hMovement > 0)
             {
-                velocity.x += movementAceleration;
+                velocity.x +=  movementAceleration;
                 if (velocity.x < 0)
                 {
                     velocity.x += movementDesaceleracion;
@@ -92,9 +122,17 @@ public class PlayerController : MonoBehaviour
         //Gravity and Jump
         if (!grounded)
         {
-
+            velocity.y -= jumpDesaceleration;
+            velocity.y = Mathf.Clamp(velocity.y, gravity, jumpSpeed);
         }
-        
+        else
+        {
+            velocity.y = 0f;
+        }
+        if (jumping)
+            {
+                velocity.y += jumpAceleration;
+            }
         velocity.x = Mathf.Clamp(velocity.x, -movementSpeed, movementSpeed);
         rb.MovePosition(rb.transform.position + (velocity * Time.deltaTime));
     }
